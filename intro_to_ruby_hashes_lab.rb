002b1d53c9044fcad66b96cd0c1c51e945aaf992
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 11:14:22 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+ new_hash{}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash{:name => "mengs"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ pioneer{:name => 'Grace Hopper'}
 end
 
 def id_generator
